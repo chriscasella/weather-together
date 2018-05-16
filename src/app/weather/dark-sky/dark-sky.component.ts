30a56624d968709605e4d29bf740fdca1ec766b5
chriscasella <chris.casella@gmail.com>
2018-05-15 21:31:43 -0400

    finally fixed dynamic weather icons

--- src/app/weather/dark-sky/dark-sky.component.ts
+++ src/app/weather/dark-sky/dark-sky.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-dark-sky',
   templateUrl: './dark-sky.component.html',
   styleUrls: ['./dark-sky.component.css']
 })
-export class DarkSkyComponent implements OnInit {
+export class DarkSkyComponent implements OnChanges {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges() {
   }
 
 }

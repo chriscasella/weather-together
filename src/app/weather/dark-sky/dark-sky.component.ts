4d0578db9d43f6e054b05f5ecb347ce164b55d7f
chriscasella <chris.casella@gmail.com>
2018-05-15 22:29:12 -0400

    ds is done for now

--- src/app/weather/dark-sky/dark-sky.component.ts
+++ src/app/weather/dark-sky/dark-sky.component.ts
@@ -4,14 +4,52 @@ import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 @Component({
   selector: 'app-dark-sky',
   templateUrl: './dark-sky.component.html',
-  styleUrls: ['./dark-sky.component.css']
+  styleUrls: ['./dark-sky.component.css', '../weather-icons.css']
 })
 export class DarkSkyComponent implements OnChanges {
   @Input() public dsCurrentForecast:any;
+  public ds:any;
+  public weatherIcon:string;
   constructor() { }
 
   ngOnChanges() {
     console.log('ds current forecast', this.dsCurrentForecast);
-  }
+    this.ds = this.dsCurrentForecast.currently;
+    this.checkWeatherIcon();
+  } 
+  
+  checkWeatherIcon() {
+    const w = this.dsCurrentForecast.currently.icon;
+    switch (w) {
+      case 'clear-day':
+        this.weatherIcon = 'wi wi-day-sunny';
+        break;
+      case 'rain':
+        this.weatherIcon = 'wi wi-rain';
+        break;
+      case 'clear-night':
+        this.weatherIcon = 'wi wi-night-clear';
+        break;
+      case 'snow':
+        this.weatherIcon = 'wi wi-snow';
+        break;
+      case 'sleet':
+        this.weatherIcon = 'wi wi-sleet';
+        break;
+      case 'windy':
+        this.weatherIcon = "wi wi-windy";
+      case 'fog':
+        this.weatherIcon = 'wi wi-fog';
+        break;
+      case 'cloudy':
+        this.weatherIcon = 'wi wi-cloudy';
+        break;
+      case 'partly-cloudy-day':
+        this.weatherIcon = 'wi wi-day-cloudy';
+        break;
+      case 'partly-cloudy-night':
+        this.weatherIcon = 'wi wi-night-cloudy';
+    }
+  };
 
 }

141035e6e8b0c45b72b913b6e8c1883df9cef98b
chriscasella <chris.casella@gmail.com>
2018-05-15 23:19:00 -0400

    going to bed, it's late

--- src/app/weather/nws/nws.component.ts
+++ src/app/weather/nws/nws.component.ts
@@ -8,10 +8,12 @@ import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 })
 export class NwsComponent implements OnChanges {
   @Input() nwsCurrentForecast:any;
+  public nws:any;
   constructor() { }
 
   ngOnChanges() {
     console.log(this.nwsCurrentForecast);
+    this.nws = this.nwsCurrentForecast.properties.periods[0];
   }
 
 }

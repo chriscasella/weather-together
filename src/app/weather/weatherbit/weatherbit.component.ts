74c323120ba30943b10c57875f283bfb392571e3
chriscasella <chris.casella@gmail.com>
2018-05-16 22:11:46 -0400

    went over limit of calls to weatherbit :(

--- src/app/weather/weatherbit/weatherbit.component.ts
+++ src/app/weather/weatherbit/weatherbit.component.ts
@@ -9,6 +9,7 @@ import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 export class WeatherbitComponent implements OnInit, OnChanges {
   @Input() wbCurrentForecast:any;
   wb:any;
+  weatherIcon:string;
   constructor() { }
 
   ngOnInit() {
@@ -19,5 +20,36 @@ export class WeatherbitComponent implements OnInit, OnChanges {
     console.log('this is the forecast', this.wbCurrentForecast)
     this.wb = this.wbCurrentForecast.data[0];
   }
+
+  checkWeatherIcon() {
+    const w = this.wbCurrentForecast.data[0].weather.code;
+    console.log('above switch');
+    switch (w) {
+      case '200' || '201' || '202' || '230' || '231' || '231' || '232' || '233':
+        this.weatherIcon = 'wi wi-thunderstorm';
+        break;
+      case '300' || '301' || '302':
+        this.weatherIcon = 'wi wi-rain';
+        break;
+      case '500' || '501' || '502' || '511' || '520' || '521' || '522':
+        this.weatherIcon = 'wi wi-showers';
+        break;
+      case '600' || '601' || '602' || '610' || '622' || '623':
+        this.weatherIcon = 'wi wi-snow';
+        break;
+      case '611' || '612':
+        this.weatherIcon = 'wi wi-sleet';
+        break;
+      case '721' || '741' || '751':
+        this.weatherIcon = 'wi wi-day-haze';
+        break;
+      case '800':
+        this.weatherIcon = 'wi wi-day-sunny';
+        break;
+      case '801' || '802' || '803' || '804':
+        this.weatherIcon = 'wi wi-cloudy';
+        break;
+    }
+  };
 
 }

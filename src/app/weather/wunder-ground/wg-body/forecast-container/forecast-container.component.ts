5f75e97d5c68f49c38bc4d5ba0aac2b8f7571cbf
chriscasella <chris.casella@gmail.com>
2018-05-19 19:39:46 -0400

    styling and backend

--- src/app/weather/wunder-ground/wg-body/forecast-container/forecast-container.component.ts
+++ src/app/weather/wunder-ground/wg-body/forecast-container/forecast-container.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-forecast-container',
   templateUrl: './forecast-container.component.html',
   styleUrls: ['./forecast-container.component.css']
 })
-export class ForecastContainerComponent implements OnInit {
-
+export class ForecastContainerComponent implements OnInit, OnChanges {
+  @Input() wgHourlyForecast;
   constructor() { }
-
+  
   ngOnInit() {
   }
+  ngOnChanges(){
+    console.log('from container', this.wgHourlyForecast)
+    this.parseDaysFromHourly();
+  }
+  public wgHourlyParsedDays = [];
+  parseDaysFromHourly(){
+    let currentDay = this.wgHourlyForecast[0].FCTTIME.weekday_name;
+    let payLoad = [];
+    for(let hour of this.wgHourlyForecast){
+      // console.log(hour);
+      if(hour.FCTTIME.weekday_name == currentDay){
+        payLoad.push(hour)
+        // console.log('pushed', payLoad)
+      } else {
+        this.wgHourlyParsedDays.push(payLoad);
+        payLoad = [];
+        currentDay = hour.FCTTIME.weekday_name;
+        payLoad.push(hour);
+        // console.log('wiped, pushed', payLoad)
+      }
+    }
+    console.log('parsed Days', this.wgHourlyParsedDays);
+  }
 
+  checkWeatherIcon(w){
+    console.log(w);
+    switch (w) {
+      case 'Chance of Flurries':
+         return 'wi wi-snow-wind';
+      case 'Rain':
+         return 'wi wi-rain';
+      case 'Chance of Rain':
+         return 'wi wi-showers';
+      case 'Chance Rain':
+         return 'wi wi-showers';
+      case 'Chance of Snow':
+         return 'wi wi-snow';
+      case 'Chance of Thunderstorms':
+         return 'wi wi-thunderstorm';
+      case 'Clear':
+         return 'wi wi-day-sunny';
+      case 'Cloudy':
+         return 'wi wi-cloudy';
+      case 'Flurries':
+         return 'wi wi-snow-wind';
+      case 'Fog':
+         return 'wi wi-fog';
+      case 'Haze':
+         return 'wi wi-day-haze';
+      case 'Mostly Cloudy':
+         return 'wi wi-cloud';
+      case 'Mostly Sunny':
+         return 'wi wi-day-sunny';
+      case 'Partly Cloudy':
+         return 'wi wi-day-cloudy';
+      case 'Partly Sunny':
+         return 'wi wi-day-cloudy';
+      case 'Freezing Rain':
+         return 'wi wi-sleet';
+      case 'Sleet':
+         return 'wi wi-sleet';
+      case 'Snow':
+         return 'wi wi-snow';
+      case 'Sunny':
+         return 'wi wi-day-sunny';
+      case 'Thunderstorm' || 'Thunderstorms':
+         return 'wi wi-thunderstorm';
+      case 'Uknown':
+         return 'wi wi-tornado';
+      case 'Scattered Clouds':
+         return 'wi wi-day-cloudy-high';
+    }
+  };
 }
--- src/app/weather/wunder-ground/wg-body/wg-body.component.ts
+++ src/app/weather/wunder-ground/wg-body/wg-body.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, OnChanges, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-wg-body',
@@ -6,6 +6,7 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
   styleUrls: ['./wg-body.component.css']
 })
 export class WgBodyComponent implements OnInit, OnChanges {
+  @Output() wgHourlyEmitter:EventEmitter<any> = new EventEmitter; 
   @Input() wgHourlyForecast:any;
 
   hourlyForecast:any;
@@ -14,8 +15,12 @@ export class WgBodyComponent implements OnInit, OnChanges {
   ngOnInit() {
   }
   ngOnChanges(){
-    this.hourlyForecast = this.wgHourlyForecast.hourly_forecast;
-    console.log('from wg body comp',this.hourlyForecast);
+    this.wgHourlyForecast = this.wgHourlyForecast.hourly_forecast;
+    // console.log('from wg body comp',this.wgHourlyForecast);
+    this.emitWgHourly();
   }
 
+  emitWgHourly(){
+    this.wgHourlyEmitter.emit(this.wgHourlyForecast);
+  }
 }
